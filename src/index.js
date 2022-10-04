b0354edfb52fb632a532475f3daad45ad8d210b3
belbarros <izabelfeigel@gmail.com>
2022-10-04 16:59:48 -0300

    Correção lint & Requisito 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
-const path = require("path");
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,7 +14,17 @@ const talkersPath = path.resolve(__dirname, './talker.json');
 app.get('/talker', async (req, res) => {
   const talkers = JSON.parse(await fs.readFile(talkersPath));
   return res.status(HTTP_OK_STATUS).json(talkers);
-})
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const id = req.params.id;
+  const talkers = JSON.parse(await fs.readFile(talkersPath));
+  const filter = talkers.find((person) => person.id === Number(id));
+  if (!filter) {
+    return res.status(404).send({ "message": "Pessoa palestrante não encontrada" });
+  }
+  return res.status(HTTP_OK_STATUS).json(filter);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
